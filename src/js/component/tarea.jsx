3a8fd6d097f50ccb5fabfbefee37bd3a5148ad01
David Nieto <xampycod@hotmail.com>
2024-08-28 17:46:35 +0000

    Todolist2.3

--- src/js/component/tarea.jsx
+++ src/js/component/tarea.jsx
@@ -76,7 +76,7 @@ const Tarea = ({ tarea, editandoTarea, eliminandoTarea }) => {
             }
 
             <div className="listaTareasBotones">
-                <button className="botonEliminar" onClick={eliminarTarea}><i className="fa-solid fa-trash"></i></button>
+                <button className="botonEliminar" onClick={() => {setTareaEliminada(!eliminandoTarea)}}><i className="fa-solid fa-trash"></i></button>
                 <button className="botonEditar" onClick={() => { setEditarTarea(!editarTarea) }}><i className="fa-solid fa-pen-to-square"></i></button>
             </div>
         </li>
